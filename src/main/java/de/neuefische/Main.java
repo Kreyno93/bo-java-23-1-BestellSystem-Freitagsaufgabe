2dc7341de4110806824dbdc367f2518151659d92
Kreyno93 <michael.wagener@neuefische.de>
2023-05-03 10:10:21 +0200

    removing unused import

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -3,7 +3,6 @@ package de.neuefische;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
